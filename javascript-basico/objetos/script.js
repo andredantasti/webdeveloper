95af0eb580951bc90e4736b8e521f5a59dab5857
Andre Dantas <andremoreirati@gmail.com>
2025-04-25 20:48:50 -0300

    entendendo e revendo objetos

---
+++ javascript-basico/objetos/script.js
@@ -0,0 +1,13 @@
+var d = new Date();
+
+console.log(d);
+
+console.log(d.getFullYear());
+
+console.log(d.getMonth());
+
+console.log(d.getDate());   
+
+console.log(d.getHours());
+
+console.log(d.getMinutes());    
