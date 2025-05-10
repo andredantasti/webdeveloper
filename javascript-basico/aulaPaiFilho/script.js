f43115650abb68f9fea4afa51524674a63ec2ad2
Andre Dantas <andremoreirati@gmail.com>
2025-05-10 20:10:36 -0300

    entendendo e manipulando elementos pai e filho

---
+++ javascript-basico/aulaPaiFilho/script.js
@@ -0,0 +1,12 @@
+
+let d1 = document.getElementById("d1");
+    console.log(d1.children);
+    
+let h1 = d1.children[0];
+console.log(h1)
+
+let paiElemento = h1.parentElement;
+console.log(paiElemento)
+
+let h3 = paiElemento.children;
+console.log(h3);
