33566d49cb26a9e9eaf3e1bb46d2cd57b409c6a8
Andre Dantas <andremoreirati@gmail.com>
2025-05-09 22:29:38 -0300

    apredendo JS intermediario

---
+++ javascript-basico/aulaEventos/script.js
@@ -0,0 +1,21 @@
+
+function h1click(){
+    let h2 = document.getElementsByTagName("h2");
+    console.log(h2);
+
+    h2[0].style.color = "red";
+}
+
+function changeH1(){
+    let input = document.getElementById("texto");
+    let h1 = document. getElementsByTagName("h1")[0];
+    console.log(h1)
+    h1.innerText = input.value;
+}
+
+function hideH1(){
+    let texto = document.getElementsByTagName("div")[0];
+    console.log(texto)
+    texto.style.display = "none"
+}
+
